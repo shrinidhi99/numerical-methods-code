d00bae632056cbac27c352c885a83d0b4dd6745c
shrinidhi99 <shrinidhi99.varna@gmail.com>
2021-02-20 23:24:56 +0530

    ran newton.py from non-linear equations

--- Python-scripts/System of non-linear equations/newton.py
+++ Python-scripts/System of non-linear equations/newton.py
@@ -1,6 +1,7 @@
 import numpy as np
 from math import inf
 
+
 def newton_raphson(F, J, X, tol, n):
     """
     Arguments
@@ -29,25 +30,39 @@ def newton_raphson(F, J, X, tol, n):
         iterations += 1
     return X
 
-f = lambda X: X[0]**2 + X[0]*X[1] + X[1]**2 - 7
-g = lambda X: X[0]**3 + X[1]**3 - 9
 
-F = lambda X: np.array([f(X), g(X)])
+def f(X): return X[0]**2 - X[1]**2 - 3
+
+
+def g(X): return X[0]**2 + X[1]**2 - 13
+
+
+def F(X): return np.array([f(X), g(X)])
 
 # partial derivatives w.r.t each xi
-fx0 = lambda X: 2*X[0] + X[1]
-fx1 = lambda X: X[0] + 2*X[1]
-gx0 = lambda X: 3*X[0]**2
-gx1 = lambda X: 3*X[1]**2
 
-J = lambda X: np.array([
+
+def fx0(X): return 2*X[0]
+
+
+def fx1(X): return -2*X[1]
+
+
+def gx0(X): return 2*X[0]
+
+
+def gx1(X): return 2*X[1]
+
+
+def J(X): return np.array([
     [fx0(X), fx1(X)],
     [gx0(X), gx1(X)]
 ])
 
+
 initial_approx = np.array([1.5, 0.5])
 tol = 1e-6
-n = 5
+n = 10
 
 newton_raphson(F, J, initial_approx, tol, n)
 
