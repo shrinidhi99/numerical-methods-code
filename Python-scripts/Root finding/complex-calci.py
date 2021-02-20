88871ac91f09c26e3e141bccf8eff42e7bbd7f73
shrinidhi99 <shrinidhi99.varna@gmail.com>
2021-02-20 20:17:41 +0530

    added a calci to calculate a,b,c values in muller's method

--- Python-scripts/Root finding/complex-calci.py
+++ Python-scripts/Root finding/complex-calci.py
@@ -7,15 +7,15 @@ def f(x):
 
 
 def muller_a(x0, x1, x2):
-    a = (x1 - x2) * (f(x0) - f(x2)) - (x0 - x2) * \
-        (f(x1) - f(x2)) / ((x0 - x2)*(x1 - x2)*(x0 - x1))
+    a = ((x1 - x2) * (f(x0) - f(x2)) - (x0 - x2) *
+         (f(x1) - f(x2))) / ((x0 - x2)*(x1 - x2)*(x0 - x1))
 
     print(f'a = {a}')
 
 
 def muller_b(x0, x1, x2):
-    b = (x0 - x2)**2 * (f(x1) - f(x2)) - (x1 - x2)**2 * \
-        (f(x0) - f(x2)) / ((x0 - x2)*(x1 - x2)*(x0 - x1))
+    b = ((x0 - x2)**2 * (f(x1) - f(x2)) - (x1 - x2)**2 *
+         (f(x0) - f(x2))) / ((x0 - x2)*(x1 - x2)*(x0 - x1))
 
     print(f'b = {b}')
 
@@ -25,15 +25,15 @@ def muller_c(x2):
     print(f'c = {c}')
 
 
-muller_a(complex(-1, 0.9),
-         complex(-1.4772872167775053, 0.7190733089686845),
-         complex(-1.4732594924196465, 0.8074951700914978))
+muller_a(complex(-1, 0),
+         complex(-1, 1),
+         complex(-1, 0.9))
 
-muller_b(complex(-1, 0.9),
-         complex(-1.4772872167775053, 0.7190733089686845),
-         complex(-1.4732594924196465, 0.8074951700914978))
+muller_b(complex(-1, 0),
+         complex(-1, 1),
+         complex(-1, 0.9))
 
-muller_c(complex(-1.4732594924196465, 0.8074951700914978))
+muller_c(complex(-1, 0.9))
 
 # x2 = -1 + 0.9j
 # x3 = -1.4772872167775053 + 0.7190733089686845j
